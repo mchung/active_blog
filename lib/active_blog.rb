9128c3a9f4d4161b5fa729a67b00c07038d0a8d3
Marc Chung <mchung@gmail.com>
2011-11-17 11:41:34 -0700

    Added the content_for variable

--- lib/active_blog.rb
+++ lib/active_blog.rb
@@ -8,6 +8,10 @@ module ActiveBlog
   mattr_accessor :blog_title
   @@blog_title = 'New ActiveBlog!'
 
+  # Yield to your layout's sidebar, i.e. :sidebar
+  mattr_accessor :blog_content_for_sidebar
+  @@blog_content_for_sidebar = nil
+
   def self.setup
     yield self
   end
