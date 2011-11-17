12f0076b6931208ab3739fec1c11be272cb90eba
Marc Chung <mchung@gmail.com>
2011-11-17 11:18:57 -0700

    Added +blog_title+ config option

--- lib/active_blog.rb
+++ lib/active_blog.rb
@@ -1,7 +1,13 @@
 require 'active_blog/engine'
 require 'active_blog/mapper'
+require 'redcarpet'
 
 module ActiveBlog
+
+  # The blog's title
+  mattr_accessor :blog_title
+  @@blog_title = 'New ActiveBlog!'
+
   def self.setup
     yield self
   end
