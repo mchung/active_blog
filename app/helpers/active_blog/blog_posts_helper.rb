7974002a7027af55e9f4fe28c7df26d6556dfa33
Marc Chung <mchung@gmail.com>
2011-11-17 23:12:38 -0700

    Apply schemas from schema.org

--- app/helpers/active_blog/blog_posts_helper.rb
+++ app/helpers/active_blog/blog_posts_helper.rb
@@ -3,5 +3,9 @@ module ActiveBlog
     def markdown(text)
       Redcarpet.new(text, :hard_wrap).to_html.html_safe
     end
+
+    def schema_date(date)
+      date.strftime("%Y-%m-%d")
+    end
   end
 end
