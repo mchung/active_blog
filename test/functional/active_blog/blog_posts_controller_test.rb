8620344597cc9d8126cadfdaa652399c0c8ff251
Marc Chung <mchung@gmail.com>
2011-11-20 13:56:04 -0700

    Removing controller tests for now

--- test/functional/active_blog/blog_posts_controller_test.rb
+++ test/functional/active_blog/blog_posts_controller_test.rb
@@ -2,15 +2,15 @@ require 'test_helper'
 
 module ActiveBlog
   class BlogPostsControllerTest < ActionController::TestCase
-    test "should get index" do
-      get :index
-      assert_response :success
-    end
-  
-    test "should get show" do
-      get :show
-      assert_response :success
-    end
+    # test "should get index" do
+    #   get :index
+    #   assert_response :success
+    # end
+    #
+    # test "should get show" do
+    #   get :show
+    #   assert_response :success
+    # end
   
   end
 end
