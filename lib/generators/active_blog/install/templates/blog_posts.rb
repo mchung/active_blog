bc09321e83c06c258b7788a57d2661733ac9b9fc
Marc Chung <mchung@gmail.com>
2011-11-20 21:40:11 -0700

    Changed blog_post_show to show

--- lib/generators/active_blog/install/templates/blog_posts.rb
+++ lib/generators/active_blog/install/templates/blog_posts.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register ActiveBlog::BlogPost do
   form :partial => 'active_blog/active_admin/blog_post_form'
   show :title => :title do
-    render 'active_blog/active_admin/blog_post_show'
+    render 'active_blog/active_admin/show'
   end
 end
