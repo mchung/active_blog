a29b719d12bbe9e6bd6c981aa1cc6b3e8bf0c25d
Marc Chung <mchung@gmail.com>
2011-11-20 21:23:16 -0700

    ActiveBlog's active_admin.js updates

---
+++ app/assets/javascripts/active_blog/active_admin.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  var $form = $('form.active_blog_blog_post');
+
+  $("#button-preview").bind("ajax:before", function() {
+    var action = $form.attr('action');
+    $form.attr('action', '/blog/_preview');
+    $form.submit();
+    $form.attr('action', action);
+    return false;
+  });
+
+  $form.bind('submit', function() {
+    if ($form.attr('action') === '/blog/_preview') {
+      console.log("In post " + this.action);
+      $.post(this.action, $(this).serialize(), null, "script");
+      return false;
+    }
+  });
+
+  // $("#new_active_blog_blog_post").live('keyup', function() {
+  //   $("#button-preview").trigger('click');
+  // });
+});
